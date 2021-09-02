aa62f197f63626f75eac246303599b710057b71a
bernardo-mr1 <devbernardomr@gmail.com>
2021-09-02 16:32:27 -0300

    feat: :sparkles: Creat new componets for study

---
+++ src/components/basics/Fragmentos.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Fragmento(props) {
+    <React.Fragment>
+        <h2>Fragmento</h2> 
+        <p>Cuidado com esse erro</p> 
+    </React.Fragment>
+}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import ReactDom from 'react-dom'
 import './index.css' //usar importaçao realitva
 import React from 'react' 
 import Primeiro from './components/basics/first'
-
+import ComParametro from './components/basics/ComParametro'
+import Fragmento from './components/basics/Fragmentos'
 
 ReactDom.render(
     <div>
-        <Primeiro>
-            
-        </Primeiro>
+        <Primeiro></Primeiro>
+        <ComParametro  
+        titulo="Situaçao do Aluno"
+        aluno="Pedro Mendes" nota={8.7} /> 
     </div>,
     document.getElementById('root') // chamada em JS puro para chamaa o ID
 )
