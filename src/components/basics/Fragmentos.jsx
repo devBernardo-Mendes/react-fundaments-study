389461719886f0d3a5fc2cf9f5e3399feef71238
bernardo-mr1 <devbernardomr@gmail.com>
2021-09-03 12:06:27 -0300

    fix: :sparkles: Add return for correct bug

--- src/components/basics/Fragmentos.jsx
+++ src/components/basics/Fragmentos.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 export default function Fragmento(props) {
-    <React.Fragment>
-        <h2>Fragmento</h2> 
-        <p>Cuidado com esse erro</p> 
-    </React.Fragment>
+    return (
+        <>
+            <h2>Fragmento</h2>
+            <p>Cuidado com esse erro</p>
+        </>
+    )
+
 }
