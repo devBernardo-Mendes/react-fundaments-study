08ae37722d27a4008613c9591fb46373911f6962
bernardo-mr1 <devbernardomr@gmail.com>
2021-09-03 12:05:26 -0300

    refactor: :memo: Update for simple code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import ReactDom from 'react-dom' 
+import ReactDom from 'react-dom'
 import './index.css' //usar importaçao realitva
-import React from 'react' 
-import Primeiro from './components/basics/first'
-import ComParametro from './components/basics/ComParametro'
-import Fragmento from './components/basics/Fragmentos'
+import React from 'react'
 
-ReactDom.render(
-    <div>
-        <Primeiro></Primeiro>
-        <ComParametro  
-        titulo="Situaçao do Aluno"
-        aluno="Pedro Mendes" nota={8.7} /> 
-    </div>,
+import App from './App'
+
+
+ReactDom.render( <
+    App / > ,
     document.getElementById('root') // chamada em JS puro para chamaa o ID
 )
