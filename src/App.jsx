e5d284b98f5690f8b800b3f494ae6d8f625a4738
bernardo-mr1 <devbernardomr@gmail.com>
2021-09-03 15:39:01 -0300

    feat:

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ComParametro from './components/basics/ComParametro'
 import Fragmento from './components/basics/Fragmentos'
 import Aleatorio from './components/basics/Aleatorio'
 import Card from './components/layout/Card'
+import Familia from './components/basics/Familia'
 
 export default () => (
 
@@ -13,22 +14,27 @@ export default () => (
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
-            <Card titulo="#04 -Desafio Aleatorio">
+
+            <Card titulo="#05 - Componentes com Filho" color="#fc0000">
+                <Familia />
+            </Card>
+
+            <Card titulo="#04 - Desafio Aleatorio" color="#090">
                 <Aleatorio min={1} max={60} />
             </Card>
 
-            <Card titulo=" #03 -Fragmento">
+            <Card titulo=" #03 - Fragmento" color="#609">
                 <Fragmento />
             </Card>
 
-            <Card titulo=" #02 -Com Parametro">
+            <Card titulo=" #02 - Com Parametro" color="#309">
                 <ComParametro
                     titulo="Situaçao do Aluno"
                     aluno="Pedro Mendes" nota={8.7}
                 />
             </Card>
 
-            <Card titulo="#01 - Primeiro">
+            <Card titulo="#01 - Primeiro" color="#098">
                 <Primeiro></Primeiro>
             </Card>
         </div>
