219c2bb0d1572e1a729407696e326cdc601b65b9
bernardo-mr1 <devbernardomr@gmail.com>
2021-09-06 14:06:00 -0300

    feat: :sparkles: date for student repetition

---
+++ src/data/alunos.js
@@ -0,0 +1,12 @@
+export default [
+    {id: 1, nome: 'Ana', nota: 9.4},
+    {id: 2, nome: 'Bia', nota: 6.1},
+    {id: 3, nome: 'Joao', nota: 8.3},
+    {id: 4, nome: 'Gui', nota: 6.9},
+    {id: 5, nome: 'Bella', nota: 7.7},
+    {id: 6, nome: 'Gomes', nota: 9.0},
+    {id: 7, nome: 'Biel', nota: 6.3},
+    {id: 8, nome: 'Barbara', nota: 7.0},
+    {id: 8, nome: 'Luiz', nota: 6.1},
+    {id: 9, nome: 'Neuza', nota: 8.5},
+] 
